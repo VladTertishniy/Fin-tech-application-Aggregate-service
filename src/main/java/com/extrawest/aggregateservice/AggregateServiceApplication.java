7734869d7d47ba60d32dd73ab449dd52720f9b10
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-06 12:33:24 +0300

    initial commit

---
+++ src/main/java/com/extrawest/aggregateservice/AggregateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.extrawest.aggregateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregateServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/extrawest/aggregateservice/AggregateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extrawest.aggregateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
