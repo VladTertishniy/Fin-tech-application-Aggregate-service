df21eac136f807b92f6357a4e4fd4cfc2cd4fc4f
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-19 20:03:47 +0300

    added AggregationSendingContainer.java and AggregationSendingOperation.java models

---
+++ src/main/java/com/extrawest/aggregateservice/model/AggregationSendingOperation.java
@@ -0,0 +1,10 @@
+package com.extrawest.aggregateservice.model;
+
+public enum AggregationSendingOperation {
+    SELL_POINT_CREATED,
+    SELL_POINT_UPDATED,
+    SELL_POINT_DELETED,
+    OPERATOR_CREATED,
+    OPERATOR_UPDATED,
+    OPERATOR_DELETED;
+}
