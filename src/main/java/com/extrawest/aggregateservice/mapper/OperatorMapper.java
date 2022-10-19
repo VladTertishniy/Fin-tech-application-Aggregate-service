c8c50d3944d9afa8e61332cc4c3f4db512d389f0
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-19 20:06:36 +0300

    added OperatorMapper.java

--- src/main/java/com/extrawest/aggregateservice/mapper/OperatorMapper.java
+++ src/main/java/com/extrawest/aggregateservice/mapper/OperatorMapper.java
@@ -7,4 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface OperatorMapper {
     OperatorResponseDto toDto (Operator operator);
+    Operator toModel (OperatorResponseDto operatorResponseDto);
 }
