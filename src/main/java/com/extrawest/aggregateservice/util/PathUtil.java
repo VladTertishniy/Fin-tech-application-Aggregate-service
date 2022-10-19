9e2d2605d5b11606025113443f0c81de33c68ca7
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-20 02:46:49 +0300

    added PathUtil.java

---
+++ src/main/java/com/extrawest/aggregateservice/util/PathUtil.java
@@ -0,0 +1,8 @@
+package com.extrawest.aggregateservice.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathUtil {
+    public static final String SELL_POINT_FULL_INFO_PATH = "/sellPointsFullInfo";
+}
